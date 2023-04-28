206957e7608ffbe2a7215075745e527fc59fbc95
RafaelCSO <rafaeljet00@gmail.com>
2023-04-28 16:20:59 -0300

    colecoes mod11 part 2

---
+++ Colecoes-mod11-exercicio-parte2/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        boolean continuar = true;
+
+        List<String> masculino = new ArrayList<>();
+        List<String> feminino = new ArrayList<>();
+
+        System.out.println("Inserir nome  -  sexo");
+        String entrada =s.nextLine();
+        String[] dados = entrada.split("-");
+
+
+    }
+}
