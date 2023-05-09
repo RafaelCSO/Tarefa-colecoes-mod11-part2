58299d84212a97b1ff50fb0c944c123caf760a47
RafaelCSO <92119405+RafaelCSO@users.noreply.github.com>
2023-05-09 14:49:22 -0300

    Update Main.java

--- Colecoes-mod11-exercicio-parte2/src/Main.java
+++ Colecoes-mod11-exercicio-parte2/src/Main.java
@@ -1,19 +1,44 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner s = new Scanner(System.in);
-        boolean continuar = true;
 
         List<String> masculino = new ArrayList<>();
         List<String> feminino = new ArrayList<>();
+        boolean continuar = true;
 
-        System.out.println("Inserir nome  -  sexo");
-        String entrada =s.nextLine();
-        String[] dados = entrada.split("-");
+        while(continuar){
+            System.out.print("Digite o nome e sexo separado por hifen: ");
+            String entradaNome = s.nextLine();
+            String[] entradaArray = entradaNome.split("-");
+            entradaArray[1] = entradaArray[1].toUpperCase().trim();
+
+            if(entradaArray[1].equalsIgnoreCase("M")){
+                masculino.add(entradaArray[0]);
+
+            }
+            else if (entradaArray[1].equalsIgnoreCase("F")){
+                feminino.add(entradaArray[0]);
+
+            }
 
 
+            System.out.println("Continuar ? Y/N");
+            String novoNome = s.nextLine();
+            if (novoNome.equalsIgnoreCase("Y")){
+                continuar = true;
+            }
+            else {
+                continuar = false;
+            }
+
+        }
+        
+        Collections.sort(masculino);
+        Collections.sort(feminino);
+        System.out.println("Nomes Masculinos Ordenados: " + Arrays.toString(masculino.toArray()));
+        System.out.println("Nomes Femininos Ordenados: " + Arrays.toString(feminino.toArray()));
     }
 }
